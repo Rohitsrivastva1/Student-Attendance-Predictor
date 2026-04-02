2394d0abaf592876a4f82545e126f5c632019add
Rohit Srivastava <70742576+Rohitsrivastva1@users.noreply.github.com>
2026-04-02 22:51:15 +0530

    Initial Commit

---
+++ Student Attendance Predictor/Student_Attendance_PredictorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Student_Attendance_PredictorApp.swift
+//  Student Attendance Predictor
+//
+//  Created by Rohit Srivastava on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Student_Attendance_PredictorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
